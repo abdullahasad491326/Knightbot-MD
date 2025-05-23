899e5b1cf0061bd6f27d75fe0e52bc4f0ede3c73
abdullahasad491326 <abdullahasad491326@gmail.com>
2025-05-23 20:52:07 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,11 +1,11 @@
 const settings = {
-  packname: 'Knight',
-  author: 'Bot',
+  packname: 'BOT',
+  author: 'ABDULLAH ASAD',
   botName: "Functional Bot",
   botOwner: 'ABDULLAH ASAD', // Your name
   ownerNumber: '923348544535', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
-  commandMode: "public",
+  commandMode: "private",
   description: "This is a bot for managing group commands and automating tasks.",
   version: "1.0.0",
 };
