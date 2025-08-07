afd82fd3052873ad634e9b583f56e560b0e3e84e
ABDULLAH ASAD <abdullahasad491326@gmail.com>
2025-08-07 19:06:28 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: '‎',
   botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'ADMIN ABDULLAH ASAD', // Your name
+  ownerNumber: '923348544535,923415039609,923216538057,923219277673', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
